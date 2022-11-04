b540b291c5adc745afd6d7c17f2c21848cc6c0e1
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-11-04 15:44:12 -0400

    Modified files

--- index.js
+++ index.js
@@ -30,12 +30,29 @@ let menu = {
 
     function onMenuListChanged() 
     {
-        const menuList = document.getElementById("menuList");
+        const menuList = document.getElementById("menuList"); //... check for entrees or desserts...
         let selectedValue = menuList.value;
+        let theList;
+        //-------------------------------------------------------------------------------------
+        if(selectedValue == "drinks"){
+            theList = menu.drinks;
+        }
+        if(selectedValue == "entrees"){
+            theList = menu.entrees;
+        }
+        if(selectedValue == "desserts"){
+            theList = menu.desserts;
+        }
 
-        let menuLength = menu.length;
-        for (let i = 0; i < menuLength; i++)
+        //-------------------------------------------------------------------------------------
+        let listLength = theList.length;
+        for (let i = 0; i < listLength; i++)
         {
-            let ()
+            let newOption = document.createElement("subItems");
+            newOption.value = menu[i].drinks;
+            newOption.value = menu [i].entrees;
+            newOption.value = menu[i].desserts;
+
+            listLength.appendChild(newOption);
         }
     }
